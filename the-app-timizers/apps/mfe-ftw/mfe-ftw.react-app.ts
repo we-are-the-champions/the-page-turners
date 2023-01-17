eb161aaba63f23c96bbfd292812380842f1e27a3
Nitsan Cohen <77798308+NitsanCohen770@users.noreply.github.com>
2023-01-17 20:17:24 +0200

    remove prerender as it's causing issues

--- the-app-timizers/apps/mfe-ftw/mfe-ftw.react-app.ts
+++ the-app-timizers/apps/mfe-ftw/mfe-ftw.react-app.ts
@@ -3,9 +3,6 @@ import { ReactAppOptions } from '@teambit/react';
 export const MfeFtwApp: ReactAppOptions = {
   name: 'mfe-ftw',
   entry: [require.resolve('./mfe-ftw.app-root')],
-  prerender: {
-    routes: ['/']
-  }
 };
 
 export default MfeFtwApp;
