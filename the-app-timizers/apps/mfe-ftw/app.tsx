0db526925cc543e4225a8972bbebde80d400119c
Nitsan Cohen <77798308+NitsanCohen770@users.noreply.github.com>
2023-01-17 20:03:05 +0200

    test github actions again

--- the-app-timizers/apps/mfe-ftw/app.tsx
+++ the-app-timizers/apps/mfe-ftw/app.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { BestPageForever } from '@we-are-the-champions/the-page-turners.pages.best-page-forever';
+import { Header } from '@we-are-the-champions/the-header-herders.header';
 
 export function MfeFtwApp() {
   return (
-    <Routes>
-      <Route path="/" element={<BestPageForever />}></Route>
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<BestPageForever />}></Route>
+      </Routes>
+    </>
   );
 }
