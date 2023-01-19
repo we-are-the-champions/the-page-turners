dca2593e87fe45bc0ed90befcbc6ac9f1c8439f2
Nitsan Cohen <77798308+NitsanCohen770@users.noreply.github.com>
2023-01-19 21:35:50 +0200

    fix test

--- the-page-turners/pages/best-page-forever/best-page-forever.spec.tsx
+++ the-page-turners/pages/best-page-forever/best-page-forever.spec.tsx
@@ -4,11 +4,9 @@ import { BestPageForeverComposition } from './best-page-forever.composition';
 
 describe('BestPageForeverComposition', () => {
   it('should render the component', () => {
-    const { getByText } = render(<BestPageForeverComposition />);
-    expect(
-      getByText(
-        'Integrating Micro Frontends with Bit and Git for Easier Collaboration'
-      )
-    ).toBeInTheDocument();
+    const { getByRole } = render(<BestPageForeverComposition />);
+    // check if the scope link 'the page turners' is rendered
+    const rendered = getByRole('link', { name: 'the page turners' });
+    expect(rendered).toBeTruthy();
   });
 });
