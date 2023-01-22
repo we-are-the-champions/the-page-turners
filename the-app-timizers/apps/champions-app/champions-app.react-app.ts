04bf7dc145b6f7294db5df5bac543b274a311f5b
Nitsan Cohen <77798308+NitsanCohen770@users.noreply.github.com>
2023-01-22 14:55:28 +0200

    update deploy options

--- the-app-timizers/apps/champions-app/champions-app.react-app.ts
+++ the-app-timizers/apps/champions-app/champions-app.react-app.ts
@@ -1,8 +1,21 @@
 import { ReactAppOptions } from '@teambit/react';
+import {
+  NetlifyOptions,
+  Netlify,
+} from '@teambit/cloud-providers.deployers.netlify';
+
+const netlifyConfig: NetlifyOptions = {
+  team: 'teambit',
+  accessToken: process.env.NETLIFY_AUTH_TOKEN as string,
+  productionSiteName: 'the-champions-app',
+  redirects: '_redirects',
+};
 
 export const MfeFtwApp: ReactAppOptions = {
   name: 'champions-app',
   entry: [require.resolve('./champions-app.app-root')],
+  deploy: Netlify.deploy(netlifyConfig),
+  favicon: require.resolve('./champions.png'),
 };
 
 export default MfeFtwApp;
